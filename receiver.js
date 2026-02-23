e645606bd15b2a674c2d877fdd35af82ddf08f23
raju-10 <86772515+raju-10@users.noreply.github.com>
2026-02-23 18:00:08 +0530

    Update license URL and authorization token in receiver.js

--- receiver.js
+++ receiver.js
@@ -31,7 +31,7 @@ playerManager.setMediaPlaybackInfoHandler((loadRequest, playbackConfig) => {
     const drmToken = customData.fastpix.tokens.drm;
 
     const licenseUrl =
-      "https://api.fastpix.io/v1/on-demand/drm/license/widevine/cd5f87a1-5fd3-49bb-b103-cc1095193f7f";
+      "https://api.fastpix.io/v1/on-demand/drm/license/widevine/cd5f87a1-5fd3-49bb-b103-cc1095193f7f?token=eyJhbGciOiJSUzI1NiJ9.eyJraWQiOiI4NzRmYjY3MS00ZmJmLTRhZGYtYTdmMy01ZTNjZTQ2OGFjMjQiLCJhdWQiOiJkcm06Y2Q1Zjg3YTEtNWZkMy00OWJiLWIxMDMtY2MxMDk1MTkzZjdmIiwiaXNzIjoiZmFzdHBpeC5pbyIsInN1YiI6IiIsImlhdCI6MTc3MTg0ODkzNSwiZXhwIjoxNzcxOTM1MzM1fQ.GjZP5NLssBiYQG43O1h-1o2zT3DiEfpbeA4gs-Qd11g1pDzrxai-ZvSlkZBxZI0W0eEEPFEhQaw53rzrnndsDYm_imNZHjraXjRAeHftyuNk_1mGi_so-2jbwqLZis1j-LMYuV4-_OEHgm9eRdPT-PmWHdPIRuAEuahtp5pq5diraeOYlY3JidTaDr7kEbFK3JBUwzoVWoetdN5hziD7qBwea0BlmiWeWF07aaQryHk7i2lMFGOVCXyu6MoU-vqZwlepnJqK63aRw3hon3Q3gViCw8nZ67G6Fi-NyjNkBx7oqA31oDrumPTECoGd_7r_j7JDYVmkgdqLLnBZMq8Qtg";
 
     console.log('Applying Widevine DRM');
 
@@ -41,7 +41,6 @@ playerManager.setMediaPlaybackInfoHandler((loadRequest, playbackConfig) => {
         licenseUrl: licenseUrl,
         headers: {
           'Content-Type': 'application/octet-stream',
-          'Authorization': `Bearer eyJhbGciOiJSUzI1NiJ9.eyJraWQiOiI4NzRmYjY3MS00ZmJmLTRhZGYtYTdmMy01ZTNjZTQ2OGFjMjQiLCJhdWQiOiJkcm06Y2Q1Zjg3YTEtNWZkMy00OWJiLWIxMDMtY2MxMDk1MTkzZjdmIiwiaXNzIjoiZmFzdHBpeC5pbyIsInN1YiI6IiIsImlhdCI6MTc3MTg0ODkzNSwiZXhwIjoxNzcxOTM1MzM1fQ.GjZP5NLssBiYQG43O1h-1o2zT3DiEfpbeA4gs-Qd11g1pDzrxai-ZvSlkZBxZI0W0eEEPFEhQaw53rzrnndsDYm_imNZHjraXjRAeHftyuNk_1mGi_so-2jbwqLZis1j-LMYuV4-_OEHgm9eRdPT-PmWHdPIRuAEuahtp5pq5diraeOYlY3JidTaDr7kEbFK3JBUwzoVWoetdN5hziD7qBwea0BlmiWeWF07aaQryHk7i2lMFGOVCXyu6MoU-vqZwlepnJqK63aRw3hon3Q3gViCw8nZ67G6Fi-NyjNkBx7oqA31oDrumPTECoGd_7r_j7JDYVmkgdqLLnBZMq8Qtg`
         }
       }
     };
